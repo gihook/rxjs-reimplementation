d7c976cecc8675967520a004425f0ed0b784edef
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2022-02-12 15:47:50 +0100

    feat: implemented take operator

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
-// operators
-export function map<T, R>(mapper: (value: T) => R): Operator<T, R> {
+export function createOperator<T, R>(
+  onNext: (
+    value: T,
+    observer: Observer<R>,
+    subscription?: Subscription,
+  ) => void,
+): Operator<T, R> {
   return function createNewObservable(
     oldObservable: Observable<T>,
   ): Observable<R> {
     return new Observable<R>((observer) => {
       const subscription = oldObservable.subscribe({
-        next: (oldValue) => {
-          const newValue = mapper(oldValue);
-          observer.next(newValue);
+        next: (value) => {
+          onNext(value, observer, subscription);
         },
         complete: () => observer.complete(),
         error: (error) => observer.error(error),
@@ -20,56 +24,50 @@ export function map<T, R>(mapper: (value: T) => R): Operator<T, R> {
       };
     });
   };
+}
+
+// operators
+export function map<T, R>(mapper: (value: T) => R): Operator<T, R> {
+  return createOperator((value, observer) => {
+    const newValue = mapper(value);
+    observer.next(newValue);
+  });
 }
 
 export function filter<T>(doesPass: (value: T) => boolean): Operator<T, T> {
-  return function createNewObservable(
-    oldObservable: Observable<T>,
-  ): Observable<T> {
-    return new Observable<T>((observer) => {
-      const subscription = oldObservable.subscribe({
-        next: (value) => {
-          if (!doesPass(value)) return;
+  return createOperator((value, observer) => {
+    if (!doesPass(value)) return;
 
-          observer.next(value);
-        },
-        complete: () => observer.complete(),
-        error: (error) => observer.error(error),
-      });
-
-      return {
-        unsubscribe: function () {
-          subscription.unsubscribe();
-        },
-      };
-    });
-  };
+    observer.next(value);
+  });
 }
 
-export function takeUntil<T>(shouldEnd: (value: T) => boolean): Operator<T, T> {
-  return function createNewObservable(
-    oldObservable: Observable<T>,
-  ): Observable<T> {
-    return new Observable<T>((observer) => {
-      const subscription = oldObservable.subscribe({
-        next: (value) => {
-          if (shouldEnd(value)) {
-            observer.complete();
-            subscription.unsubscribe();
-            return;
-          }
+export function take<T>(numberOfItems: number): Operator<T, T> {
+  let count = 0;
 
-          observer.next(value);
-        },
-      });
+  return createOperator((value, observer, subscription) => {
+    if (count == numberOfItems) {
+      subscription?.unsubscribe();
+      observer.complete();
+      return;
+    }
 
-      return {
-        unsubscribe: function () {
-          subscription.unsubscribe();
-        },
-      };
-    });
-  };
+    count++;
+    observer.next(value);
+  });
+}
+
+export function scan<T, R>(
+  reducer: (a: T, c: R) => R,
+  initialValue: R,
+): Operator<T, R> {
+  let currentValue = JSON.parse(JSON.stringify(initialValue)) as R;
+
+  return createOperator((value, observer, _subscription) => {
+    const newValue = reducer(value, currentValue);
+    observer.next(newValue);
+    currentValue = newValue;
+  });
 }
 
 export interface Observer<T> {
@@ -113,6 +111,13 @@ export class Observable<T> {
     operator2: Operator<A, B>,
     operator3: Operator<B, R>,
   ): Observable<R>;
+
+  pipe<A, B, C, R>(
+    operator1: Operator<T, A>,
+    operator2: Operator<A, B>,
+    operator3: Operator<B, C>,
+    operator4: Operator<C, R>,
+  ): Observable<R>;
   pipe() {
     let observable = this;
 
@@ -144,13 +149,16 @@ const interval$ = interval();
 
 const test$ = interval$.pipe(
   filter((num) => num % 4 == 0),
-  takeUntil((num) => num > 200),
-  map((num) => `Hello ${num}`),
+  take(10),
+  scan((a, c) => a + c, 0),
+  map((sum) => `current sum ${sum}`),
 );
 
 const subscription = test$.subscribe({
-  next: (value: any) => {
-    console.log({ value });
+  next: (value) => {
+    console.log(value);
   },
   complete: () => console.log("test$ completed"),
 });
+
+setTimeout(() => subscription.unsubscribe(), 300);
